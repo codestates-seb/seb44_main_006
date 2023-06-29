518b6a028e019ed50a6841b0c8c0dd7ff2602855
novice-hero <highero700@gmail.com>
2023-06-29 10:34:14 +0900

    init

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,68 @@
+const prettierConfig = require("./_prettier");
+
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "react",
+    "react-hooks",
+    "import",
+  ],
+  rules: {
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off",
+    "prettier/prettier": ["error", prettierConfig],
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "external",
+          "sibling",
+          "parent",
+          "internal",
+          "builtin",
+          "object",
+          "type",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>HI</>;
+}
+
+export default App;
