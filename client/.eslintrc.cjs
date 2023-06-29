8d61e337d154c91eb3be86b9c0875c9f5826aa06
novice-hero <highero700@gmail.com>
2023-06-29 11:42:14 +0900

    init: eslint 오류 잡기, 폴더 구조 세팅

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,68 +1,68 @@
-const prettierConfig = require('./_prettier.cjs');
+const prettierConfig = require("./_prettier.cjs");
 
 module.exports = {
   extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    project: './tsconfig.json',
-    sourceType: 'module',
+    project: "./tsconfig.json",
+    sourceType: "module",
     createDefaultProgram: true,
   },
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    'import/resolver': {
+    "import/resolver": {
       typescript: {},
     },
   },
   plugins: [
-    '@typescript-eslint/eslint-plugin',
-    'react',
-    'react-hooks',
-    'import',
+    "@typescript-eslint/eslint-plugin",
+    "react",
+    "react-hooks",
+    "import",
   ],
   rules: {
-    'class-methods-use-this': 'off',
-    'import/prefer-default-export': 'off',
-    'prettier/prettier': ['error', prettierConfig],
-    'react/jsx-props-no-spreading': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/require-default-props': 'off',
-    'react/function-component-definition': [
-      'error',
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off",
+    "prettier/prettier": ["error", prettierConfig],
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "react/function-component-definition": [
+      "error",
       {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
       },
     ],
-    'import/first': 'error',
-    'import/newline-after-import': 'error',
-    'import/no-duplicates': 'error',
-    'import/order': [
-      'error',
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/order": [
+      "error",
       {
         groups: [
-          'external',
-          'sibling',
-          'parent',
-          'internal',
-          'builtin',
-          'object',
-          'type',
-          'index',
+          "external",
+          "sibling",
+          "parent",
+          "internal",
+          "builtin",
+          "object",
+          "type",
+          "index",
         ],
-        'newlines-between': 'always',
+        "newlines-between": "always",
       },
     ],
   },
