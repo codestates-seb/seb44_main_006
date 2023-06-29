8aae4b1c9604b1dd441604415f76206e99c2a096
BlancCarte <yusung0924@naver.com>
2023-06-29 10:46:03 +0900

    init: BE 초기설정

---
+++ server/src/main/java/com/seb_main_006/SebMain006Application.java
@@ -0,0 +1,13 @@
+package com.seb_main_006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebMain006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebMain006Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb_main_006/SebMain006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb_main_006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebMain006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
