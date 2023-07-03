45688be182c3df0474492ae1cbcd246850738bc3
jieun419 <crg1050@naver.com>
2023-07-03 13:33:05 +0900

    design: 유저 이미지 스타일 작업 유저 이미지 스타일 및 props에 따라 다르게 보여주는 작업 했습니다.

---
+++ client/src/components/ui/user/UserInfoMy.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+import cssToken from '../../../styles/cssToken';
+
+type UserInfoStyle = {
+  size?: string;
+};
+
+interface UserInfo {
+  styles?: UserInfoStyle;
+  src?: string;
+  alt?: string;
+}
+
+export const UserImgContainer = styled.div<UserInfoStyle>`
+  border-radius: ${cssToken.BORDER['rounded-full']};
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => (props.size ? props.size : '5.3125rem')};
+`;
+
+export const Img = styled.img`
+  object-fit: cover;
+  width: ${cssToken.WIDTH['w-full']};
+  height: ${cssToken.HEIGHT['h-full']};
+`;
+
+const UserInfoMy = ({ styles, src }: UserInfo) => {
+  return (
+    <UserImgContainer {...styles}>
+      <Img src={src} alt="userImg" />
+    </UserImgContainer>
+  );
+};
+
+export default UserInfoMy;
