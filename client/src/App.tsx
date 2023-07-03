af3d21da9af97cb0a20da9762370d33e1e9ff6dc
jieun419 <crg1050@naver.com>
2023-07-03 21:34:04 +0900

    style: App.tsx 내용 복구 테스트를 위한 App.tsx 작성한 내용 복구했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,5 @@
-import styled from 'styled-components';
-
-import GlobalStyle from './GlobalStyle';
-import ContensCard from './components/ui/cards/ContentsCard';
-
-const CardListContainer = styled.article`
-  display: flex;
-  gap: 0.9375rem;
-  padding: 3.125rem 6.25rem;
-  flex-wrap: wrap;
-`;
-
 const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <CardListContainer>
-        <ContensCard
-          userName="최지은"
-          title="전부타비켜엘사불러도날못식혀쇳뿔 달고뛰어일 직선쉽"
-          text="전부타비켜엘사불러도날못식혀쇳뿔 달고뛰어일 직선쉽"
-          likeCount="60"
-          tag={['엘사', '제발']}
-        />
-        <ContensCard
-          title="전부타비켜엘사불러도날못식혀쇳뿔 달고뛰어일 직선쉽"
-          text="전부타비켜엘사불러도날못식혀쇳뿔 달고뛰어일 직선쉽"
-          likeCount="88"
-          tag={['티켓팅', '여름']}
-        />
-      </CardListContainer>
-    </>
-  );
+  return <>HI</>;
 };
 
 export default App;
