7ec633a00081e60d28b35b881a62b236692e1ecd
jieun419 <crg1050@naver.com>
2023-07-03 15:36:00 +0900

    design: App 초기 상태로 복구 App 초기 상태로 복구했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,5 @@
-import UserInfoPfp from './components/ui/UserInfoPfp';
-
 const App = () => {
-  return (
-    <>
-      <UserInfoPfp
-        styles={{
-          size: '10.75rem',
-        }}
-        src="https://lh3.googleusercontent.com/a/AAcHTtdraz9lk5yB6ffVrzH-TL1MzMQgzuzAWVfOtO687Co0M9E=s83-c-mo"
-      />
-      <UserInfoPfp
-        styles={{
-          size: '5.3125rem',
-        }}
-        src="https://lh3.googleusercontent.com/a/AAcHTtdraz9lk5yB6ffVrzH-TL1MzMQgzuzAWVfOtO687Co0M9E=s83-c-mo"
-      />
-    </>
-  );
+  return <>HI</>;
 };
 
 export default App;
