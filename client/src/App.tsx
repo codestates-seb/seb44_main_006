44de3a0de4636492ec0e2554895ed92a37c50a0f
jieun419 <crg1050@naver.com>
2023-07-03 23:29:42 +0900

    style: App.tsx 복구 테스트 작업으로 인한 App.tsx 내용 복구

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,5 @@
-import ContensCard from './components/ui/cards/ContentsCard';
-import LocationCard from './components/ui/cards/LocationCard';
-import MapLocationCard from './components/ui/cards/MapLocationCard';
-
 const App = () => {
-  return (
-    <>
-      <LocationCard
-        title="장생당 약국"
-        category="약국"
-        address="서울 강남구 테헤란로84길 17"
-        phone="02-558-5476"
-      />
-      <MapLocationCard indexNum={1} location="구림 빌라" />
-      <MapLocationCard indexNum={2} location="구림 11" />
-      <MapLocationCard indexNum={3} location="제발.." />
-    </>
-  );
+  return <>HI</>;
 };
 
 export default App;
