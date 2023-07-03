a4343b2acb06b2e0e3b4b5e751f152776f951adc
jieun419 <crg1050@naver.com>
2023-07-03 14:11:24 +0900

    rename: 파일명 변경 파일명, 컴포넌트 직관적으로 변경했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import UserInfo from './components/ui/UserInfo';
+import UserInfoPfp from './components/ui/UserInfoPfp';
 
 const App = () => {
   return (
     <>
-      <UserInfo
+      <UserInfoPfp
         styles={{
           size: '10.75rem',
         }}
         src="https://lh3.googleusercontent.com/a/AAcHTtdraz9lk5yB6ffVrzH-TL1MzMQgzuzAWVfOtO687Co0M9E=s83-c-mo"
       />
-      <UserInfo
+      <UserInfoPfp
         styles={{
           size: '5.3125rem',
         }}
