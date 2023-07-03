0223990be7a1ba0a91b268d6d8aa7feb766e6bc9
Ryu jisu <wltn1418@gmail.com>
2023-07-03 20:20:18 +0900

    refactor: css속성재사용

--- client/src/components/ui/text/SubTitle.tsx
+++ client/src/components/ui/text/SubTitle.tsx
@@ -1,14 +1,9 @@
 import { styled } from 'styled-components';
 
 import { Props, TextStyleT } from '../../../types/type';
-import cssToken from '../../../styles/cssToken';
+import { TextStyle } from '../../../styles/styles';
 
-const H3 = styled.h3<TextStyleT>`
-  font-size: ${(props) => (props.size ? props.size : '1rem')};
-  color: ${(props) => (props.color ? props.color : 'black')};
-  font-weight: ${(props) =>
-    props.weight ? props.weight : cssToken.FONT_WEIGHT.bold};
-`;
+const H3 = styled(TextStyle)``;
 
 const SubTitle = ({
   children,
--- client/src/components/ui/text/Text.tsx
+++ client/src/components/ui/text/Text.tsx
@@ -1,16 +1,11 @@
 import { styled } from 'styled-components';
 
 import { Props, TextStyleT } from '../../../types/type';
-import cssToken from '../../../styles/cssToken';
+import { TextStyle } from '../../../styles/styles';
 
-const P = styled.p<TextStyleT>`
-  //Todo 멘토님에게 styles 코드 겹치는 거 어떻게 해결할지
+const P = styled(TextStyle).attrs({ as: 'p' })`
   display: flex;
   align-items: center;
-  font-size: ${(props) => (props.size ? props.size : '1rem')};
-  color: ${(props) => (props.color ? props.color : 'black')};
-  font-weight: ${(props) =>
-    props.weight ? props.weight : cssToken.FONT_WEIGHT.medium};
 `;
 
 const Text = ({
