715601960f32e9208c9dc916dd8a9172e24a828c
jieun419 <crg1050@naver.com>
2023-07-03 19:33:04 +0900

    design: 스타일일 작업 props 없이 스타일 작업

--- client/src/components/ui/thumbnail/ThumbnailBox.tsx
+++ client/src/components/ui/thumbnail/ThumbnailBox.tsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components';
+
+import cssToken from '../../../styles/cssToken';
+
+const ThumbnailContainer = styled.div`
+  position: relative;
+  border-radius: ${cssToken.BORDER['rounded-s']};
+  overflow: hidden;
+  width: ${cssToken.WIDTH['w-full']};
+  height: 0;
+  padding-bottom: 50%;
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  object-fit: cover;
+  width: ${cssToken.WIDTH['w-full']};
+  height: ${cssToken.HEIGHT['h-full']};
+`;
+
 const ThumbnailBox = () => {
-  return <>dfdf</>;
+  return (
+    <ThumbnailContainer>
+      <Img src="https://www.sungsimdangmall.co.kr/data/sungsimdang/goods/sungsimdang/big/202310454292163523295.jpg" />
+    </ThumbnailContainer>
+  );
 };
 
 export default ThumbnailBox;
