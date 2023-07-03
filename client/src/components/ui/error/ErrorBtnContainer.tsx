a96e6c503a5ed175cf5ceac099e917b0b3c3ae89
Ryu jisu <wltn1418@gmail.com>
2023-07-03 13:10:22 +0900

    design: error status 들어왔을 때 error page

---
+++ client/src/components/ui/error/ErrorBtnContainer.tsx
@@ -0,0 +1,15 @@
+import Text from '../text/Text';
+
+const ErrorBtnContainer = () => {
+  return (
+    <Text
+      styles={{
+        color: 'white',
+      }}
+    >
+      메인으로 가기
+    </Text>
+  );
+};
+
+export default ErrorBtnContainer;
