d8a17df17cc394bef4332a9a93e96a613f3ce0e6
Ryu jisu <wltn1418@gmail.com>
2023-07-04 14:50:11 +0900

    fix: array 변경 시 linepath 리렌더링안되는 오류 해결

--- client/src/components/map/Polyline.tsx
+++ client/src/components/map/Polyline.tsx
@@ -1,22 +1,29 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const Polyline = ({ linePos }: { linePos: { lat: number; lng: number }[] }) => {
   const map = useSelector((state) => state.map.map);
 
-  const linePath = linePos.map(
-    (pos: { lat: number; lng: number }) =>
-      new kakao.maps.LatLng(pos.lat, pos.lng)
-  );
+  useEffect(() => {
+    const linePath = linePos.map(
+      (pos: { lat: number; lng: number }) =>
+        new kakao.maps.LatLng(pos.lat, pos.lng)
+    );
+    const polyline = new kakao.maps.Polyline({
+      path: linePath,
+      strokeWeight: 2,
+      strokeColor: '#FFAE00',
+      strokeOpacity: 0.7,
+      strokeStyle: 'solid',
+    });
+    polyline.setMap(map);
 
-  const polyline = new kakao.maps.Polyline({
-    path: linePath,
-    strokeWeight: 2,
-    strokeColor: '#FFAE00',
-    strokeOpacity: 0.7,
-    strokeStyle: 'solid',
-  });
-
-  polyline.setMap(map);
+    return () => {
+      if (polyline) {
+        polyline.setMap(null);
+      }
+    };
+  }, [map, linePos]);
 
   return null;
 };
