fd1c5a9a23621b5009b2fe5d8f75467613e70bb9
BlancCarte <yusung0924@naver.com>
2023-07-04 16:10:06 +0900

    feat: 일정생성 구현 완료

---
+++ server/src/main/java/com/seb_main_006/domain/course/dto/CourseData.java
@@ -0,0 +1,15 @@
+package com.seb_main_006.domain.course.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class CourseData {
+    private String courseDday;
+
+    private String courseTitle;
+
+    private String courseContent;
+
+    private String courseThumbnail;
+}
