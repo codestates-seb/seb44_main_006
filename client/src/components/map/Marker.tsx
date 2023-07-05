231f8c9ad45310e384ddae07ba087a5974bacc28
novice-hero <highero700@gmail.com>
2023-07-05 22:42:07 +0900

    chore: defaultOptions 경로 수정

--- client/src/components/map/Marker.tsx
+++ client/src/components/map/Marker.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 import { Props } from '../../types/type';
-import defaultOptions from '../../utils/constant';
+import defaultOptions from '../../utils/constant/constant';
 import { markerActions } from '../../store/marker-slice';
 import { RootState } from '../../store';
 
@@ -63,7 +63,7 @@ const Marker = ({ lat, lng, id, img, idx, children }: MarkerT) => {
         kakao.maps.event.removeListener(marker, 'click', setMarkerId);
       }
     };
-  }, [map, lat, lng, markerId, dispatch, id, img]);
+  }, [map, lat, lng, markerId, dispatch, id, img, idx]);
 
   if (children) {
     return <div>{children}</div>;
