e754f0b1ba4a31fe9ec47069008a36b587f0064d
BlancCarte <yusung0924@naver.com>
2023-07-05 16:02:17 +0900

    feat: 커뮤니티게시글 생성 구현완료

---
+++ server/src/main/java/com/seb_main_006/domain/post/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.seb_main_006.domain.post.repository;
+
+import com.seb_main_006.domain.course.entity.Course;
+import com.seb_main_006.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findByCourse(Course course);
+}
