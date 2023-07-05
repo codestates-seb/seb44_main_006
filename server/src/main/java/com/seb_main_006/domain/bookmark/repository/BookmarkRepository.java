59bdebd0b57edf907b911ed525dae6defb0e2bc3
BlancCarte <yusung0924@naver.com>
2023-07-05 20:49:22 +0900

    feat: 즐겨찾기 구현 완료

--- server/src/main/java/com/seb_main_006/domain/bookmark/repository/BookmarkRepository.java
+++ server/src/main/java/com/seb_main_006/domain/bookmark/repository/BookmarkRepository.java
@@ -1,10 +1,15 @@
 package com.seb_main_006.domain.bookmark.repository;
 
 import com.seb_main_006.domain.bookmark.entity.Bookmark;
+import com.seb_main_006.domain.course.entity.Course;
 import com.seb_main_006.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Book;
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     Integer countBookmarksByMember(Member member);
+    Optional<Bookmark> findBookmarkByCourse(Course course);
 }
