c92e1857417a347aa57645b646d582b6d0c2fe3d
jieun419 <crg1050@naver.com>
2023-07-05 13:05:18 +0900

    style: header, footer 추가 header, footer 추가했으나 예시 화면으로 삭제 예정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 
 const App = () => {
   return (
     <>
       <Header isMainPage />
+      <Footer />
     </>
   );
 };
