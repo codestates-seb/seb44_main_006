7127c8235593b378fef6941ddff6ad1095beffcf
jieun419 <crg1050@naver.com>
2023-07-05 11:02:54 +0900

    design: 기본 버전 디자인 로그인 전, 기본 버전 디자인

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,7 +1,48 @@
+import { styled } from 'styled-components';
+
+import cssToken from '../../styles/cssToken';
+import LogoBlack from '../../assets/common_img/logo_black.svg';
+import WhiteButton from '../ui/button/WhiteButton';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${cssToken.SPACING['gap-10']} ${cssToken.SPACING['gap-24']};
+  background: ${cssToken.COLOR.white};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: ${cssToken.WIDTH['w-full']};
+  box-shadow: ${cssToken.SHADOW['shadow-lg']};
+`;
+
+const LogoBox = styled.h1`
+  width: 150px;
+`;
+
+const LogoImg = styled.img`
+  width: ${cssToken.WIDTH['w-full']};
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  gap: ${cssToken.SPACING['gap-10']};
+`;
+
 const Header = () => {
-  return(
-    <>header</>
-  )
-}
+  return (
+    <HeaderContainer>
+      <LogoBox>
+        <a href="/">
+          <LogoImg src={LogoBlack} alt="logo-harumate" />
+        </a>
+      </LogoBox>
+      <BtnBox>
+        <WhiteButton>로그인</WhiteButton>
+      </BtnBox>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
