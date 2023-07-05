04c6ae422e4ab51bb625ef5ef613d89562591cd9
jieun419 <crg1050@naver.com>
2023-07-05 15:35:07 +0900

    design: main page design main page design

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,5 +1,58 @@
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import mainImg from '../assets/mainImg.png';
+import cssToken from '../styles/cssToken';
+
+const MainContainer = styled.main`
+  display: flex;
+`;
+
+const SectionBox = styled.section`
+  height: ${cssToken.HEIGHT['h-screen']};
+  width: ${cssToken.WIDTH['w-screen']};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MainLink = styled(Link)`
+  text-decoration: none;
+  outline: none;
+  font-size: 80px;
+  font-weight: ${cssToken.FONT_WEIGHT.bold};
+`;
+
+const CommunitySection = styled(SectionBox)`
+  background: ${cssToken.COLOR.white};
+  flex: 1;
+  > a {
+    color: ${cssToken.COLOR['point-900']};
+  }
+`;
+
+const ScheduleSection = styled(SectionBox)`
+  background-image: url(${mainImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  flex: 2;
+  > a {
+    color: ${cssToken.COLOR.white};
+  }
+`;
+
 const Main = () => {
-  return <>main</>;
+  return (
+    <MainContainer>
+      <CommunitySection>
+        <MainLink to="/">Community</MainLink>
+      </CommunitySection>
+      <ScheduleSection>
+        <MainLink to="/">Schedule</MainLink>
+      </ScheduleSection>
+    </MainContainer>
+  );
 };
 
 export default Main;
