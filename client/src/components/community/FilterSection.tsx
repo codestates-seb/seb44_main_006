068203992f8086d7a68d1a5c46c9be193f2a9d4e
Ryu jisu <wltn1418@gmail.com>
2023-07-06 23:17:08 +0900

    feat: card click 시 상세페이지로 이동

--- client/src/components/community/FilterSection.tsx
+++ client/src/components/community/FilterSection.tsx
@@ -1,10 +1,12 @@
 import { styled } from 'styled-components';
 import { useInView } from 'react-intersection-observer';
+import { useNavigate } from 'react-router-dom';
 
 import ContensCard from '../ui/cards/ContentsCard';
 import cssToken from '../../styles/cssToken';
 import { CardWrapper, FlexDiv } from '../../styles/styles';
 import { Props } from '../../types/type';
+import useMovePage from '../../hooks/useMovePage';
 
 const FilterWrapper = styled.div`
   width: 100%;
@@ -22,13 +24,17 @@ const FilterContainer = styled(FlexDiv)`
 `;
 
 const FilterSection = ({ children }: { children: Props['children'] }) => {
+  const navigate = useNavigate();
   const [ref, inView] = useInView();
+  const moveToDetail = (id: number | undefined) => {
+    if (id) navigate(`/community/${id}`);
+  };
   return (
     <FilterWrapper>
       <FilterContainer>{children}</FilterContainer>
       {/* Todo 리액트쿼리 데이터로 변경하기 */}
       <CardWrapper>
-        <ContensCard />
+        <ContensCard onClick={moveToDetail} id={1} />
         <ContensCard />
         <ContensCard />
         <ContensCard />
