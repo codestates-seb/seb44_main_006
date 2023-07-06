ac396a32c91c0a673c82895280ee3ed50a8e8c5d
jieun419 <crg1050@naver.com>
2023-07-06 21:54:06 +0900

    feat: 회원 정보 API 주소 proxy 설정 회원 정보 API 주소 proxy 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth/members': {
+        target:
+          'http://ec2-52-78-64-106.ap-northeast-2.compute.amazonaws.com:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/auth/logout': {
+        target:
+          'http://ec2-52-78-64-106.ap-northeast-2.compute.amazonaws.com:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
