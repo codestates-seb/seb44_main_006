d45c82f767c7ca9276c88b33b06c03fd3bc9ea29
jieun419 <crg1050@naver.com>
2023-07-10 02:43:52 +0900

    design: 1차 반응형 작업

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -13,6 +13,10 @@ import CursorPointer from '../components/ui/cursor/cursorPointer';
 const MainContainer = styled.main`
   cursor: none;
   display: flex;
+  flex-direction: column-reverse;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 const SectionBox = styled.section`
@@ -27,7 +31,7 @@ const MainLink = styled(Link)`
   cursor: none;
   text-decoration: none;
   outline: none;
-  font-size: 80px;
+  font-size: 50px;
   font-weight: ${cssToken.FONT_WEIGHT.bold};
   > span {
     display: block;
@@ -36,17 +40,28 @@ const MainLink = styled(Link)`
   &:hover span {
     display: none;
   }
+  @media (min-width: 768px) {
+    font-size: 50px;
+  }
+
+  @media (min-width: 1280px) {
+    font-size: 60px;
+  }
+
 `;
 
 const CommunitySection = styled(SectionBox)`
   background: ${cssToken.COLOR.white};
-  flex: 1;
   transition: 0.3s;
+  height: 100vh;
   > a {
     color: ${cssToken.COLOR['point-900']};
     &:hover::after {
       content: '커뮤니티';
     }
+  }
+  @media (min-width: 768px) {
+    flex: 1;
   }
 `;
 
@@ -55,13 +70,15 @@ const ScheduleSection = styled(SectionBox)`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  flex: 2;
   transition: 0.3s;
   > a {
     color: ${cssToken.COLOR.white};
     &:hover::after {
       content: '일정 등록';
     }
+  }
+  @media (min-width: 768px) {
+    flex: 2;
   }
 `;
 
