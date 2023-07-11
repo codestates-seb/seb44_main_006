30e0afc7e42144e36c9a6335334135c31cb0a62c
jieun419 <crg1050@naver.com>
2023-07-12 02:02:02 +0900

    refactor: 타입 오류 수정

--- client/src/pages/mypage/MyPage.tsx
+++ client/src/pages/mypage/MyPage.tsx
@@ -1,10 +1,9 @@
 import { styled } from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
-import cssToken from '../../styles/cssToken';
+import { RootState } from '../../store';
+// import cssToken from '../../styles/cssToken';
 import { FlexDiv } from '../../styles/styles';
 import { GetMyList } from '../../apis/api';
 import UserInfoBox from '../../components/mypage/UserInfoBox';
@@ -18,16 +17,9 @@ const Wrapper = styled(FlexDiv)`
   row-gap: 7.75rem;
 `;
 
-const FixedDiv = styled.article`
-  position: fixed;
-  right: ${cssToken.SPACING['gap-40']};
-  bottom: ${cssToken.SPACING['gap-40']};
-`;
-
 const MyPage = () => {
-  const [memNickname, setMemNickname] = useState('');
   const userAuthInfo = useSelector(
-    (state: RootState): boolean => state.userAuth.userInfo
+    (state: RootState) => state.userAuth.userInfo
   );
 
   const { data: userMemData } = useQuery(['userInof'], () => GetMyList());
