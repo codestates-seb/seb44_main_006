db38ece569c6b07612f5ac5a9378a33aedb66df1
jieun419 <crg1050@naver.com>
2023-07-12 01:51:06 +0900

    fix: 타입 오류 및 상태 전달 수정

--- client/src/pages/mypage/MyPage.tsx
+++ client/src/pages/mypage/MyPage.tsx
@@ -1,5 +1,44 @@
+import { styled } from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import cssToken from '../../styles/cssToken';
+import { FlexDiv } from '../../styles/styles';
+import { GetMyList } from '../../apis/api';
+import UserInfoBox from '../../components/mypage/UserInfoBox';
+
+const Wrapper = styled(FlexDiv)`
+  margin-top: 77px;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 6.5rem;
+  row-gap: 7.75rem;
+`;
+
+const FixedDiv = styled.article`
+  position: fixed;
+  right: ${cssToken.SPACING['gap-40']};
+  bottom: ${cssToken.SPACING['gap-40']};
+`;
+
 const MyPage = () => {
-  return <>MyPage</>;
+  const [memNickname, setMemNickname] = useState('');
+  const userAuthInfo = useSelector(
+    (state: RootState): boolean => state.userAuth.userInfo
+  );
+
+  const { data: userMemData } = useQuery(['userInof'], () => GetMyList());
+
+  console.log(userAuthInfo);
+  console.log(userMemData);
+  return (
+    <Wrapper>
+      <UserInfoBox />
+    </Wrapper>
+  );
 };
 
 export default MyPage;
