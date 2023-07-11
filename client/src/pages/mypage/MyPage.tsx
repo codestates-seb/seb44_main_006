8a08f8b9ab8eb31563611c5424c5b1fbd120c02c
jieun419 <crg1050@naver.com>
2023-07-11 23:06:26 +0900

    remove: 배포를 위한 파일 정리

--- client/src/pages/mypage/MyPage.tsx
+++ client/src/pages/mypage/MyPage.tsx
@@ -1,23 +1,5 @@
-import { styled } from 'styled-components';
-
-import { FlexDiv } from '../../styles/styles';
-import UserInfoBox from '../../components/mypage/UserInfoBox';
-
-const Wrapper = styled(FlexDiv)`
-  margin-top: 77px;
-  width: 100%;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 6.5rem;
-  row-gap: 7.75rem;
-`;
-
 const MyPage = () => {
-  return (
-    <Wrapper>
-      <UserInfoBox />
-    </Wrapper>
-  );
+  return <>MyPage</>;
 };
 
 export default MyPage;
