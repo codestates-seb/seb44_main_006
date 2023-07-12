57fd522c7b150d94453279ee7db35e0bf3b8d158
Ryu jisu <wltn1418@gmail.com>
2023-07-12 13:48:13 +0900

    fix: courseId type 변경

--- client/src/types/type.ts
+++ client/src/types/type.ts
@@ -59,7 +59,7 @@ export interface IEventButtonStyle extends IButtonStyle {
 }
 
 export type LikeBookMarkButtonT = IEventButtonStyle & {
-  courseId?: number;
+  courseId: number;
 };
 
 export type PlacesSearchResult = PlacesSearchResultItem[];
