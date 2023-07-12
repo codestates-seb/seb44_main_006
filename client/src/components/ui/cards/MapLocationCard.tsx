79b34cbd12e29a8e41f274a4e18e9494cb7330a5
novice-hero <highero700@gmail.com>
2023-07-12 17:30:35 +0900

    feat: 일정 카드에 드래그 가능한 것을 나타내는 아이콘 추가

--- client/src/components/ui/cards/MapLocationCard.tsx
+++ client/src/components/ui/cards/MapLocationCard.tsx
@@ -10,6 +10,7 @@ import Button from '../button/Button';
 import Trash from '../../../assets/Trash';
 import { scheduleListActions } from '../../../store/scheduleList-slice';
 import { markerActions } from '../../../store/marker-slice';
+import ThreeLine from '../../../assets/ThreeLine';
 
 const MapLocationCardContainer = styled.section`
   display: flex;
@@ -53,13 +54,21 @@ const LocationCard = styled.div<{ selected?: boolean }>`
   align-items: center;
   flex: 1;
   width: ${cssToken.WIDTH['w-full']};
-  padding: ${cssToken.SPACING['gap-24']};
+  padding: ${cssToken.SPACING['gap-24']} ${cssToken.SPACING['gap-12']}
+    ${cssToken.SPACING['gap-24']} ${cssToken.SPACING['gap-16']};
   ${CardCommonBox}
 `;
 
 const LocationText = styled.p`
   font-size: ${cssToken.TEXT_SIZE['text-18']};
   font-weight: ${cssToken.FONT_WEIGHT.medium};
+`;
+
+const RightButtonArea = styled.section`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MapLocationCard = ({ indexNum, location, id }: MapLocationCardInfo) => {
@@ -89,13 +98,16 @@ const MapLocationCard = ({ indexNum, location, id }: MapLocationCardInfo) => {
         }}
       >
         <LocationText>{location}</LocationText>
-        <Button
-          onClick={() => {
-            if (id) handleDelete(id);
-          }}
-        >
-          <Trash style={{ iconWidth: 16, iconHeight: 18 }} />
-        </Button>
+        <RightButtonArea>
+          <ThreeLine style={{ iconWidth: 17, iconHeight: 18 }} />
+          <Button
+            onClick={() => {
+              if (id) handleDelete(id);
+            }}
+          >
+            <Trash style={{ iconWidth: 16, iconHeight: 18 }} />
+          </Button>
+        </RightButtonArea>
       </LocationCard>
     </MapLocationCardContainer>
   );
