8611e032361a1311424ce5321269560b54ca65cd
Ryu jisu <wltn1418@gmail.com>
2023-07-13 15:14:48 +0900

    fix: key 추가

--- client/src/components/community/skeleton/SkeletonCardContainer.tsx
+++ client/src/components/community/skeleton/SkeletonCardContainer.tsx
@@ -1,9 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import SkeletonContentCard from '../../skeleton/SkeletonContentCard';
 
 const SkeletonCardContainer = ({ length }: { length: number }) => {
   const arr = [];
   for (let index = 0; index < length; index += 1) {
-    arr.push(<SkeletonContentCard />);
+    arr.push(<SkeletonContentCard key={uuidv4()} />);
   }
   return arr;
 };
--- client/src/components/community/skeleton/SkeletonLocationContainer.tsx
+++ client/src/components/community/skeleton/SkeletonLocationContainer.tsx
@@ -1,9 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import SkeletonLocationCard from '../../skeleton/SkeletonLocationCard';
 
 const SkeletonLocationContainer = ({ length }: { length: number }) => {
   const arr = [];
   for (let index = 0; index < length; index += 1) {
-    arr.push(<SkeletonLocationCard />);
+    arr.push(<SkeletonLocationCard key={uuidv4()} />);
   }
   return arr;
 };
