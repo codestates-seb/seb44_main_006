560aa43e5c289d27f60d50b425280257614ec4f5
Ryu jisu <wltn1418@gmail.com>
2023-07-15 20:48:02 +0900

    design: modal 반응형

--- client/src/components/ui/modal/Modal.tsx
+++ client/src/components/ui/modal/Modal.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { Props } from '../../../types/type';
 import CloseButton from '../button/CloseButton';
+import cssToken from '../../../styles/cssToken';
 
 interface IModalContainer {
   width?: string;
@@ -22,6 +23,10 @@ const ModalWrapper = styled.div`
   width: 100vw;
   height: 100vh;
   z-index: 1000;
+
+  @media screen and (max-width: 768px) {
+    align-items: end;
+  }
 `;
 
 const Backdrop = styled.div`
@@ -47,6 +52,62 @@ const ModalContainer = styled.section<IModalContainer>`
   gap: ${(props) => props.gap};
   background-color: #ffffff;
   z-index: 1002;
+
+  @media screen and (max-width: 768px) {
+    height: 15.5625rem;
+    border-top-left-radius: ${cssToken.BORDER['rounded-md']};
+    border-top-right-radius: ${cssToken.BORDER['rounded-md']};
+    justify-content: space-around;
+    font-weight: ${cssToken.FONT_WEIGHT.medium};
+
+    > div {
+      padding-top: ${cssToken.SPACING['gap-50']};
+      padding-left: ${cssToken.SPACING['gap-20']};
+      padding-right: ${cssToken.SPACING['gap-20']};
+      align-items: center;
+    }
+
+    p {
+      display: flex;
+      justify-content: center;
+      font-size: ${cssToken.TEXT_SIZE['text-32']};
+      font-weight: ${cssToken.FONT_WEIGHT.medium};
+    }
+
+    button {
+      height: 3rem;
+    }
+
+    .gray {
+      width: 10rem;
+    }
+
+    .skyblue {
+      width: 10rem;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    p {
+      font-size: ${cssToken.TEXT_SIZE['text-24']};
+    }
+
+    button {
+      height: 2.1875rem;
+    }
+
+    .gray {
+      padding-left: 1.25rem;
+      padding-right: 1.25rem;
+      width: 8.5rem;
+    }
+
+    .skyblue {
+      padding-left: 1.25rem;
+      padding-right: 1.25rem;
+      width: 8.5rem;
+    }
+  }
 `;
 
 const CloseButtonDiv = styled.div`
