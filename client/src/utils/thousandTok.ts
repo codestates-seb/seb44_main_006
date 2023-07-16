775625b520a70c16638783bad53f2e7a71b7165b
Ryu jisu <wltn1418@gmail.com>
2023-07-16 21:03:18 +0900

    fix: 기본값 제거

--- client/src/utils/thousandTok.ts
+++ client/src/utils/thousandTok.ts
@@ -1,6 +1,6 @@
 const thousandTok = (value: number | undefined) => {
   if (value === undefined) return 'invalid';
-  if (value > 1000) return `${(10300 / 1000).toFixed(1)}k`;
+  if (value > 1000) return `${(value / 1000).toFixed(1)}k`;
   return value;
 };
 
