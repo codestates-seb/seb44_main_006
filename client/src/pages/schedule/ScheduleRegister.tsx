181d63bca4b36d50285e9ec69c2a454ce0676339
novice-hero <highero700@gmail.com>
2023-07-20 01:26:50 +0900

    hotfix: 경로 못찾는 거 수정

--- client/src/pages/schedule/ScheduleRegister.tsx
+++ client/src/pages/schedule/ScheduleRegister.tsx
@@ -5,7 +5,6 @@ import { useQuery } from '@tanstack/react-query';
 
 import { IScheduleListItem, PlacesSearchResultItem } from '../../types/type';
 import { RootState } from '../../store';
-import ScheduleBox from '../../components/schedule/ScheduleBox';
 import cssToken from '../../styles/cssToken';
 import { MarkerOff } from '../../components/map/index';
 import ScheduleCreateModal from '../../components/schedule/ScheduleCreateModal';
@@ -23,6 +22,7 @@ import useModifyCheck from '../../hooks/useModifyCheck';
 import { GetCourse } from '../../apis/api';
 import { PostReadT } from '../../types/apitype';
 import { scheduleListActions } from '../../store/scheduleList-slice';
+import ScheduleBox from '../../components/schedule/ScheduleBox';
 
 const KakaoMap = lazy(() => import('../../components/map/KakaoMap'));
 const Marker = lazy(() => import('../../components/map/Marker'));
