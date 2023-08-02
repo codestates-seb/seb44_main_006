bdef0aa2cb0f4e15d1e161902d8e7dca45ba3dcf
Minhyeong Song <alsuddl25@gmail.com>
2023-08-02 11:36:40 +0900

    fix: RefreshToken 재발급 로직 수정 - Redis에 저장된 RefreshToken이 없을 경우(signature 외의 값이 변조된 상태의 토큰)에 대해 예외 처리 (만료 예외 발생) - RefreshToken을 재발급할 때 Redis에 기존 RefreshToken 삭제 후 새로 저장

--- server/src/main/java/com/seb_main_006/global/auth/redis/RefreshToken.java
+++ server/src/main/java/com/seb_main_006/global/auth/redis/RefreshToken.java
@@ -1,9 +1,6 @@
 package com.seb_main_006.global.auth.redis;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
@@ -11,6 +8,7 @@ import java.util.List;
 
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @RedisHash(value = "refresh", timeToLive = 1209600)
